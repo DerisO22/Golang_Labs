9f3d81a6ef8538670750ed61bc948be4f50fa7b4
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-01-22 10:12:54 -0500

    go lab 1 set up

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Leet-Code Problem - isPalindrome
+	var i int
+    fmt.Print("Enter a Number: ")
+	fmt.Scan(&i)
+
+	fmt.Println("Your number is:", i * 10)
+}
