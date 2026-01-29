3a7a7a882834f88c5492480d1cd5c6bbcd9d8034
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-01-29 17:09:58 -0500

    Forgot to add the header

--- PAs/PA1/pa1.go
+++ PAs/PA1/pa1.go
@@ -1,3 +1,27 @@
+/*
+Author:                   Deris C. O’Malley
+Class:                    CSI-380-01
+Assignment:               PA 1
+Date Assigned:            29, January
+Due Date:                 5, February, 11:00AM
+
+Description:
+This program will ask the users for their name and date of
+birth, then give them a fortune message. This program will
+also tell the user’s age, zodiac sign, and fortune message.
+
+Certification of Authenticity:
+I certify that this is entirely my own work,except where I have given fully-documented
+References to the work of others. I understand the definition and consequences of
+Plagiarism and acknowledge that the assessor of this assignment may, for the purpose
+of assessing this assignment:
+-Reproduce this assignment and provide a copy to another member of academic staff;
+and/or
+- Communicate a copy of this assignment to a plagiarism checking service (which
+May then retain a copy of this assignment on its database for the purpose of future
+Plagiarism checking)
+*/
+
 package main
 
 import (
